39fea66dfdb73d8249f216368c3c250532e51297
Curslo <richardkisivii@gmail.com>
2025-03-21 16:10:23 +0300

    Configures eslint for linting the whole project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,35 +3,20 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+
 export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ["**/*.{ts}"],
     languageOptions: {
-      globals: globals.node, 
-      parser: tseslint.parser,
       parserOptions: {
-        project: "./tsconfig.json", 
+        project: "./tsconfig.json",
         sourceType: "module",
         ecmaVersion: "latest",
       },
     },
-    plugins: {
-      js,
-      "@typescript-eslint": tseslint.plugin, 
-    },
-    extends: [
-      "eslint:recommended", 
-      "plugin:@typescript-eslint/recommended", 
-      "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      "plugin:prettier/recommended",
-    ],
-    rules: {
-      "quotes": ["error", "double"],
-      "semi": ["error", "always"],
-      "no-console": "warn", 
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-explicit-any": "warn", 
-    },
   },
+  tseslint.configs.recommended,
 ]);
